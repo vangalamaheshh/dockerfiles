78d0b3ded2d50b8200a9636e206ad1858d6b1f6d
Mahesh Vangala <vangalamaheshh@gmail.com>
2017-07-31 15:23:15 -0400

    added php5-curl

--- bioifx/i2b2/webserver/i2b2_config_data.js
+++ bioifx/i2b2/webserver/i2b2_config_data.js
@@ -5,7 +5,7 @@
 	// THESE ARE ALL THE DOMAINS A USER CAN LOGIN TO
 	lstDomains: [
 		{ domain: "i2b2demo",
-		  name: "i2b2demo",
+		  name: "UMassdemo",
 		  urlCellPM: "http://CORESERVER:9090/i2b2/services/PMService/",
 		  allowAnalysis: true,
 		  //installer: "/webclient/plugin_installer/",
