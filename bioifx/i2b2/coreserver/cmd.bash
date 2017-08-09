3c2002c5a981ed6fd283f31b014f073478f19290
Mahesh Vangala <vangalamaheshh@gmail.com>
2017-08-09 16:26:37 -0400

    added ontology cell to dockerfile

--- bioifx/i2b2/coreserver/cmd.bash
+++ bioifx/i2b2/coreserver/cmd.bash
@@ -5,6 +5,8 @@ cd /opt/jboss/i2b2/i2b2-core-server/edu.harvard.i2b2.server-common
 ant clean dist deploy jboss_pre_deployment_setup
 cd /opt/jboss/i2b2/i2b2-core-server/edu.harvard.i2b2.pm
 ant -f master_build.xml clean build-all deploy
+cd /opt/jboss/i2b2/i2b2-core-server/edu.harvard.i2b2.ontology
+ant -f master_build.xml clean build-all deploy
 ${JBOSS_HOME}/bin/standalone.sh -b 0.0.0.0
 
 wait
