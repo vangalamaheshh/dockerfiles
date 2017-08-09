904301b3d051c4d3a8c1d2f36c5d37e617f82187
Mahesh Vangala <vangalamaheshh@gmail.com>
2017-08-09 15:12:38 -0400

    added webclient

---
+++ bioifx/i2b2/webserver/i2b2_config_data.client.js
@@ -0,0 +1,16 @@
+{
+	urlProxy: "index.php",
+	urlFramework: "js-i2b2/",
+	//-------------------------------------------------------------------------------------------
+	// THESE ARE ALL THE DOMAINS A USER CAN LOGIN TO
+	lstDomains: [
+		{ domain: "i2b2demo",
+		  name: "UMassDemo",
+		  urlCellPM: "http://CORE_SERVER:9090/i2b2/services/PMService/",
+		  allowAnalysis: true,
+		  //installer: "/webclient/plugin_installer/",
+		  debug: false
+		}
+	]
+	//-------------------------------------------------------------------------------------------
+}
