d4d0776c6f02d14b81fd8f28360dc67d79553adb
Mahesh Vangala <vangalamaheshh@gmail.com>
2017-08-09 23:27:59 -0400

    installation of all cells is completed with demo data

--- bioifx/i2b2/coreserver/cmd.bash
+++ bioifx/i2b2/coreserver/cmd.bash
@@ -9,6 +9,12 @@ cd /opt/jboss/i2b2/i2b2-core-server/edu.harvard.i2b2.ontology
 ant -f master_build.xml clean build-all deploy
 cd /opt/jboss/i2b2/i2b2-core-server/edu.harvard.i2b2.crc
 ant -f master_build.xml clean build-all deploy
+cd /opt/jboss/i2b2/i2b2-core-server/edu.harvard.i2b2.workplace
+ant -f master_build.xml clean build-all deploy
+cd /opt/jboss/i2b2/i2b2-core-server/edu.harvard.i2b2.fr
+ant -f master_build.xml clean build-all deploy
+cd /opt/jboss/i2b2/i2b2-core-server/edu.harvard.i2b2.im
+ant -f master_build.xml clean build-all deploy
 ${JBOSS_HOME}/bin/standalone.sh -b 0.0.0.0
 
 wait
