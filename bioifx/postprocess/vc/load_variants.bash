764d1e9ea467f3398508fb522fc574501019089c
Mahesh Vangala <vangalamaheshh@gmail.com>
2017-09-17 14:08:11 -0400

    added cmds to log

--- bioifx/postprocess/vc/load_variants.bash
+++ bioifx/postprocess/vc/load_variants.bash
@@ -2,33 +2,51 @@
 
 # ENV variables required
 CLOUDSDK_CORE_PROJECT="synergist-170903"
+BQ_GENOMICS_DATASET="genomics_api"
 
 # activate account
 gcloud auth activate-service-account --key-file=${gmx_file} 
 
 # if sample_name dataset exists - delete it
-for dataset_id in $(gcloud alpha genomics datasets list | grep ${sample_name} | grep -oP "^\S+"); do
+for dataset_id in $(gcloud alpha genomics datasets list | grep ${project_id} | grep -oP "^\S+"); do
   echo Y | gcloud alpha genomics datasets delete ${dataset_id}
 done
 
 # create dataset
-dataset_id=$(gcloud alpha genomics datasets create --name=${sample_name} 2>&1 | grep -oP "id:\s+(\d+)" | sed -e "s/id:\s*//g")
+dataset_cmd="gcloud alpha genomics datasets create --name=${project_id} 2>&1 | grep -oP \"id:\s+(\d+)\" | sed -e \"s/id:\s*//g\""
+echo "Dataset command: ${dataset_cmd}" >&2 
+dataset_id=$(${dataset_cmd})
 
 # create variantset
-variantset_id=$(gcloud alpha genomics variantsets create --dataset-id=${dataset_id} --name=${sample_name} 2>&1 | grep -oP "id: \d+" | head -1 | sed -e "s/id:\s*//g")
+variantset_cmd="gcloud alpha genomics variantsets create --dataset-id=${dataset_id} --name=${project_id} 2>&1 | grep -oP \"id: \d+\" | head -1 | sed -e \"s/id:\s*//g\""
+echo "Variantset command: ${variantset_cmd}" >&2
+variantset_id=$(${variantset_cmd})
 
 # import variants
-var_op_id=$(gcloud alpha genomics variants import --variantset-id=${variantset_id} --source-uris "${vcf_file}" 2>&1 | grep -oP "name:\s+(.+)" | sed -e "s/name:\s*//g")
+# first, make sure you have comma separated vcf file list
+vcf_file_list=$(echo "${vcf_file_list}" | tr "\n" "," | tr " " "," | sed -e "s/,$//")
+echo "vcf_file_list: ${vcf_file_list}" >&2
+
+variant_cmd="gcloud alpha genomics variants import --variantset-id=${variantset_id} --source-uris=${vcf_file_list} 2>&1 | grep -oP \"name:\s+(.+)\" | sed -e \"s/name:\s*//g\""
+echo "variants import command: ${variant_cmd}" >&2
+var_op_id=$(${variant_cmd})
 
 # create bq dataset if not exists
-bq_exists=$(bq ls | grep "${project_id}")
+bq_exists=$(bq ls | grep "${BQ_GENOMICS_DATASET}")
 
-# if project id - bq dataset does not exist; create it
+# if bq dataset does not exist; create it
 if [ -z $bq_exists ]; then
-  bq mk ${project_id}
+  bq mk "${BQ_GENOMICS_DATASET}"
 fi
 
-table_name=${sample_name}
+table_name="${project_id}"
+
+# if table exists - drop it
+table_exists=$(bq ls "${BQ_GENOMICS_DATASET}" | grep "${table_name}" )
+
+if [ ! -z $table_exists ]; then
+  bq rm -t -f "${BQ_GENOMICS_DATASET}.${table_name}"
+fi
 
 # keep checking until variant operation completes
 while [ $(gcloud alpha genomics operations describe "${var_op_id}"  | grep "done:" | sed -e "s/done:\s*//g") == "false" ]; do 
@@ -36,7 +54,7 @@ while [ $(gcloud alpha genomics operations describe "${var_op_id}"  | grep "done
 done
 
 # export variants to bq table
-bq_op_id=$(gcloud alpha genomics variantsets export ${variantset_id} ${table_name} --bigquery-dataset ${project_id} 2>&1 | grep -oP "name:\s+(.+)" | sed -e "s/name:\s*//g")
+bq_op_id=$(gcloud alpha genomics variantsets export ${variantset_id} ${table_name} --bigquery-dataset ${BQ_GENOMICS_DATASET} 2>&1 | grep -oP "name:\s+(.+)" | sed -e "s/name:\s*//g")
 
 # keep checking until bq operation completes
 while [ $(gcloud alpha genomics operations describe "${bq_op_id}"  | grep "done:" | sed -e "s/done:\s*//g") == "false" ]; do                  sleep 600; #sleep for 10min 
