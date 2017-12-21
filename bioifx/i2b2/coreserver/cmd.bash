55dfd1bec37d15b0007ff6be8f53885ae0f3b9ee
Mahesh Vangala <vangalamaheshh@gmail.com>
2017-12-21 10:36:50 -0500

    back to stable

--- bioifx/i2b2/coreserver/cmd.bash
+++ bioifx/i2b2/coreserver/cmd.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#/bin/bash /opt/jboss/i2b2/install_i2b2_cells.bash
+/bin/bash /opt/jboss/i2b2/install_i2b2_cells.bash
 cd /opt/jboss/i2b2/i2b2-core-server/edu.harvard.i2b2.server-common
 ant clean dist deploy jboss_pre_deployment_setup
 cd /opt/jboss/i2b2/i2b2-core-server/edu.harvard.i2b2.pm
