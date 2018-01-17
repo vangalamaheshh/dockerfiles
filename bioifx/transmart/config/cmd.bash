9630204e9ba77903c787ff6956800cd893728c88
Mahesh Vangala <vangalamaheshh@gmail.com>
2018-01-17 00:55:38 -0500

    working docker

---
+++ bioifx/transmart/config/cmd.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# start solr
+make -C /usr/local/bin/transmart/transmart-data/solr start >& /usr/local/bin/solr.log
+
+# data loading into oracle
+cd /usr/local/bin/transmart/transmart-data
+source vars
+make -j4 oracle
+
+wait
